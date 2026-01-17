b611e33e05116cdd4d13663d65639e75b89dc582
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-17 19:18:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return """
+    <!DOCTYPE html>
+    <html lang="ar">
+    <head>
+        <meta charset="UTF-8">
+        <title>Telegram Web App</title>
+    </head>
+    <body style="text-align:center;font-family:Arial">
+        <h1>✅ الويب شغال بنجاح</h1>
+        <p>تم فتح التطبيق من داخل تلجرام 🚀</p>
+    </body>
+    </html>
+    """
