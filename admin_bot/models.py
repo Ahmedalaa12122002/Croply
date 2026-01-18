ae0e98dffe775e69f5ef8b0692f7af2108909bc6
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-18 19:41:35 +0200

    Update models.py

--- admin_bot/models.py
+++ admin_bot/models.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, BigInteger, String, Boolean, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(BigInteger, unique=True, index=True, nullable=False)
+    username = Column(String(64))
+    first_name = Column(String(64))
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
