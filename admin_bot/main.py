80f56082a7e42c5bfa2a8567497fd1ebd0609576
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-18 19:52:56 +0200

    Update main.py

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -5,11 +5,13 @@ from telegram.ext import (
 )
 from config import BOT_TOKEN
 from handlers.start import start
+from handlers.callbacks import admin_callbacks
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(admin_callbacks))  # ⭐ المهم
 
     print("👑 Admin Bot is running")
     app.run_polling()
