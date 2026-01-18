2114927d4da01046aa2be77133bead82e283003a
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-18 20:56:23 +0200

    Update main.py

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -1,38 +1,25 @@
+# admin_bot/main.py
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
-    CallbackQueryHandler,
     MessageHandler,
-    ContextTypes,
+    CallbackQueryHandler,
     filters
 )
-
 from config import BOT_TOKEN
 from handlers.start import start
 from handlers.callbacks import admin_callbacks
 from handlers.messages import handle_messages
 
-
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    # /start
     app.add_handler(CommandHandler("start", start))
-
-    # أزرار Inline Keyboard (CallbackQuery)
     app.add_handler(CallbackQueryHandler(admin_callbacks))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_messages))
 
-    # استقبال الرسائل النصية (مثل ID المستخدم)
-    app.add_handler(
-        MessageHandler(
-            filters.TEXT & ~filters.COMMAND,
-            handle_messages
-        )
-    )
-
-    print("👑 Admin Bot is running...")
+    print("👑 Admin Bot is running")
     app.run_polling()
-
 
 if __name__ == "__main__":
     main()
