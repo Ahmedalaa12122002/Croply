7f8b4aeab0ce2ee292b4b9c88b0d7bf896da0576
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-18 18:07:13 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,7 @@
 import os
 from sqlalchemy.ext.asyncio import create_async_engine
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL is not set")
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_async_engine(
     DATABASE_URL,
