50f4282ae8daf1a3cf3af2a7b669316f6e480154
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-18 18:17:38 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,31 @@
 import os
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
+
+# تحويل الرابط إلى async
+DATABASE_URL = DATABASE_URL.replace(
+    "postgresql://", "postgresql+asyncpg://"
+)
 
 engine = create_async_engine(
     DATABASE_URL,
-    echo=True,
+    echo=False,
     pool_pre_ping=True
 )
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
