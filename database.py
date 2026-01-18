1a8d0d086ea948a1b277a32098b26a4d6e3c4fce
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-18 17:16:08 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,13 @@
 import os
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_async_engine(
     DATABASE_URL,
-    echo=False
+    echo=True,
+    pool_pre_ping=True
 )
-
-AsyncSessionLocal = sessionmaker(
-    engine,
-    class_=AsyncSession,
-    expire_on_commit=False
-)
-
-Base = declarative_base()
