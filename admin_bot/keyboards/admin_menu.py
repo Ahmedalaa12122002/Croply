6e70a5cf6ace3c66e52b3d6f4ddbc6d6901736e0
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-18 19:47:06 +0200

    Update admin_menu.py

--- admin_bot/keyboards/admin_menu.py
+++ admin_bot/keyboards/admin_menu.py
@@ -1,1 +1,13 @@
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+def admin_menu():
+    keyboard = [
+        [
+            InlineKeyboardButton("🆕 أحدث المستخدمين", callback_data="latest_users")
+        ],
+        [
+            InlineKeyboardButton("🔍 البحث عن مستخدم", callback_data="search_user")
+        ]
+    ]
+    return InlineKeyboardMarkup(keyboard)
