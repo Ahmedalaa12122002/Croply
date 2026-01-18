e22cd1587a1b176cb167d14f5ba49de68263b1ac
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-18 20:39:51 +0200

    Update config.py

--- admin_bot/config.py
+++ admin_bot/config.py
@@ -1,5 +1,10 @@
-
+# admin_bot/config.py
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+ROLE_OWNER = "owner"
+ROLE_ADMIN = "admin"
+ROLE_MODERATOR = "moderator"
