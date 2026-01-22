60f5e960dab2b3b5d73086aabd481a70e462b793
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-22 05:32:49 +0200

    Update permissions.py

--- admin_bot/permissions.py
+++ admin_bot/permissions.py
@@ -1,1 +1,17 @@
+# permissions.py
+from enum import Enum
 
+class Role(Enum):
+    BRONZE = 1
+    GOLD = 2
+    PLATINUM = 3
+    PROFESSOR = 4
+    OWNER = 5
+
+# مؤقتًا (لاحقًا هنربطه بـ DB)
+ADMINS = {
+    5102387551: Role.OWNER
+}
+
+def get_admin_role(user_id: int):
+    return ADMINS.get(user_id)
