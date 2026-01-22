f5e34d4d8312d2f783c7343c26e1cf00e40dadab
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-22 05:55:24 +0200

    Update config.py

--- admin_bot/config.py
+++ admin_bot/config.py
@@ -1,22 +1,7 @@
-# security.py
-from config import OWNER_ID, EMERGENCY_LOCK
-from permissions import get_admin_role, Role
+# config.py
+BOT_TOKEN = "PUT_ADMIN_BOT_TOKEN_HERE"
 
-def is_owner(user_id: int) -> bool:
-    return user_id == OWNER_ID
+OWNER_ID = 5102387551
 
-def is_admin(user_id: int) -> bool:
-    return get_admin_role(user_id) is not None
-
-def check_access(user_id: int, required_role: Role) -> bool:
-    if is_owner(user_id):
-        return True
-
-    if EMERGENCY_LOCK:
-        return False
-
-    role = get_admin_role(user_id)
-    if not role:
-        return False
-
-    return role.value >= required_role.value
+TEST_MODE = True        # 🧪
+EMERGENCY_LOCK = False # 🚨
